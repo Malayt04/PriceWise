83e5e11c28e944250cd774c49d5650cc96633f3c
Malay Tiwari <malayt04@gmail.com>
2024-10-02 21:04:56 +0530

    added gmail service

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -80,13 +80,17 @@ export async function generateEmailBody(
 }
 
 const transporter = nodemailer.createTransport({
+  pool: true,
+  host:'smtp.gmail.com',
   service: 'gmail',
+  port: 2525,
+  secure:false,
   auth: {
-    user: 'malayt04@gmail.com',
+    user: 'EMAIL_ADDRESS',
     pass: process.env.EMAIL_PASSWORD,
   },
   maxConnections: 1
-});
+})
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
   const mailOptions = {
@@ -94,12 +98,11 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
-  };
-
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ', info.response);
-  } catch (error) {
-    console.error('Error sending email: ', error);
   }
+
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if(error) return console.log("error", error);
+    
+    console.log('Email sent: ', info);
+  })
 }
